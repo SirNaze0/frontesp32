a2533884d7f1186e41cc93b79d73531dd15bade8
Gianmarco <161788198+SirNaze0@users.noreply.github.com>
2024-12-12 02:56:49 -0500

    a

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // URLs de la API
-const API_URL = 'https://backesp32-jr64.onrender.com';
+const API_URL = 'backesp32-jr64.onrender.com';
 const ENDPOINTS = {
     uploadEstudiantes: '/upload_estudiantes',
     getEstudiantes: '/get_estudiantes',
